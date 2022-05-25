43ea4ce2bdfb266099b6b67ef10aedcdc3d5c11f
Josh Yee <joshyee@gmail.com>
2022-05-25 15:47:02 -0700

    added drag and drop and click to choose files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import DragAndDrop from './components/DragAndDrop'
+import { ProgressBar } from "react-bootstrap"
 
 const App = () => {
 
 
 
 
+
+  // if progroess is null 
+    // show drag and drop box
+  
+  // if progress is filled 
+    //show progress bar instead of drag and drop
+
+  // after progress bar is done set uploaded to true
+
+  // if uploaded is true, show the Drag and Drop Results
+
+
   return (
     <div className="box">
       <p className="box__title">UPLOAD YOUR IMAGE</p>
       <p className="box__title2">File should be jpeg, png...</p>
+
       <DragAndDrop />
 
 
--- src/components/DragAndDrop.js
+++ src/components/DragAndDrop.js
@@ -5,24 +5,25 @@ import axiosInstance from '../utils/axiosInstance';
 import { ProgressBar } from "react-bootstrap"
 
 
-const DragAndDrop = props => {
+const DragAndDrop = (props) => {
 
   const fileInput = useRef(null);
   const [previewUrl, setPreviewUrl] = useState('')
-
-  const [progress, setProgress] = useState()
-
+  const [progress, setProgress] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [uploadMessage, setUploadMessage] = useState(null)
 
   const handleFile = file => {
       //put validations here
       setPreviewUrl(URL.createObjectURL(file))
-    //   let formData = new FormData()
+      
+      let formData = new FormData()
 
-    //   formData.append('file', selectedFiles[0])
+      formData.append('file', file)
 
-      axiosInstance.post('/upload_file', file, {
+      axiosInstance.post('/upload_file', formData, {
           headers: {
-              "Content-Type": "image/png",
+              "Content-Type": "multipart/form-data",
           },
           onUploadProgress: data => {
               setProgress(Math.round((100 * data.loaded) / data.total))
@@ -30,48 +31,77 @@ const DragAndDrop = props => {
       })
   }
 
-  const handleOndragOver = e => {
-      e.preventDefault()
-  }
 
   const handleOnDrop = e => {
-    e.preventDefault()
-    e.stopPropagation()
+        e.preventDefault()
+        e.stopPropagation()
 
-    let imageFile = e.dataTransfer.files[0]
-    console.log("first", imageFile)
-    handleFile(imageFile)
-}
+        let imageFile = e.dataTransfer.files
+        setPreviewUrl(URL.createObjectURL(e.dataTransfer.files[0]))
+
+        let formData = new FormData()
+
+        formData.append("file", imageFile[0])
+        axiosInstance.post("/upload_file", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+            onUploadProgress: data => {
+                //Set the progress value to show the progress bar
+                setProgress(Math.round((100 * data.loaded) / data.total))
+                if (data.loaded === data.total) {
+                    console.log("its done")
+                }
+            },        
+        })
+    }
+
+    const handleOndragOver = e => {
+        e.preventDefault()
+    }
+
+    let myInput = null;
+    const copyToClipboard = () => {
+      myInput.select();
+      document.execCommand("copy");
+      setMessage("Copied link to clipboard!")
+      setTimeout(() => {
+          setMessage(null)
+      }, 3000)
+    };
+
 
     
   return (
       <div className="box-Wrapper">
-        
-        {previewUrl ? <img alt="upload_image" className="dropped-image" src={previewUrl}/> : 
+           
+
+        {!previewUrl ?    
             <div> 
                 <div className='drag-drop-zone' onDrop={handleOnDrop} onDragOver={handleOndragOver} onClick = { () => fileInput.current.click()} >
-                    <img alt="upload_image" src={uploadImageIcon} />
+                    <img className="dragbox__icon" alt="upload_image" src={uploadImageIcon} />
+                    <p className="dragBox__title">Click to Select or</p><p className="dragBox__title2">Drag and Drop Image</p>
+                    
+                        <input 
+                            type="file" 
+                            accept='image/*' 
+                            ref={fileInput} hidden
+                            onChange={e => handleFile(e.target.files[0])}
 
-                    <p className="dragBox__title">Click to Select</p>
-                    <p className="dragBox__title">or</p>
-                    <p className="dragBox__title">Drag and Drop image here...</p>
-
-                    <input 
-                        type="file" 
-                        accept='image/*' 
-                        ref={fileInput} hidden 
-                        onChange={e => handleFile(e.target.files[0])}
-                        name="file"
-
-                    />
-
+                        />
                 </div>
-
             </div>
+            : 
+            <>
+                <img alt="upload_image" className="dropped-image" src={previewUrl} />
+                <ProgressBar className="progress-bar" now={progress} label={`${progress}%`} />
+                <input className="copy-btn__input"readOnly value={previewUrl} onClick={copyToClipboard} ref={(ref) => myInput = ref} />
+                <button className="copy-btn" onClick={copyToClipboard}>Copy Link</button>
+                <p className="copybox__message">{message}</p>
+            </>
+        }
 
-            }
 
-        {progress && <ProgressBar now={progress} label={`${progress}%`} />}
 
 
 
