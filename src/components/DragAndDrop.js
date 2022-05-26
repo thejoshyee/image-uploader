0cc4c8a05406791f28e6474988b4ac875087e122
Josh Yee <joshyee@gmail.com>
2022-05-26 11:14:25 -0700

    added file size and type validation

--- src/components/DragAndDrop.js
+++ src/components/DragAndDrop.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import '../App.css'
 import uploadImageIcon from '../upload-image.svg'
 import axiosInstance from '../utils/axiosInstance';
-import { ProgressBar } from "react-bootstrap"
+import { ProgressBar, Alert } from "react-bootstrap"
 import checkmark from '../assets/checkmark.png'
 
 
@@ -14,33 +14,76 @@ const DragAndDrop = (props) => {
   const [message, setMessage] = useState(null)
   const [uploaded, setUploaded] = useState(false)  
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState()
 
   useEffect(() => {
     if(previewUrl) {
         setTimeout(() => {
             setIsLoading(false)
             setUploaded(true)
-        }, 4000)
+        }, 3000)
     }
   },[previewUrl])
 
+  const validateSize = (file) => {
+    let maxfilesize = 1024 * 1024
+    let filesize = file.size
+    
+    if ( filesize > maxfilesize ) {  
+      return false
+    } else {
+        return true
+    }
+  }
+
   const handleFile = file => {
       //put validations here
-      setPreviewUrl(URL.createObjectURL(file))
+      let validation = validateSize(file)
+      if (!validation) {
+          setError('Please choose a file that is 1 mb or less.')
+          setTimeout(() => {
+              setError('')
+          },4000)
+          return false
+      } else {
+
+        setPreviewUrl(URL.createObjectURL(file))
       
-      let formData = new FormData()
+        let formData = new FormData()
+  
+        formData.append('file', file)
+  
+        axiosInstance.post('/upload_file', formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+            onUploadProgress: data => {
+                setProgress(Math.round((100 * data.loaded) / data.total))
+                setIsLoading(true)
+            }
+        })
+        .catch(error => {
+          const { code } = error?.response?.data
+          switch (code) {
+              case "FILE_MISSING":
+                  setError("Please select a file before uploading")
+                  break
+              case "LIMIT_FILE_SIZE":
+                  setError("File size is too large. Please upload files below 1MB!")
+                  break
+              case "INVALID_TYPE":
+                  setError(
+                      "This file type is not supported! Only .png, .jpg, and .jpeg files are allowed!"
+                  )
+                  break
 
-      formData.append('file', file)
-
-      axiosInstance.post('/upload_file', formData, {
-          headers: {
-              "Content-Type": "multipart/form-data",
-          },
-          onUploadProgress: data => {
-              setProgress(Math.round((100 * data.loaded) / data.total))
-              setIsLoading(true)
+              default:
+                  setError("Sorry, something went wrong. Please try again later.")
+                  break
           }
       })
+
+      }
   }
 
 
@@ -48,24 +91,65 @@ const DragAndDrop = (props) => {
         e.preventDefault()
         e.stopPropagation()
 
-        let imageFile = e.dataTransfer.files
-        setPreviewUrl(URL.createObjectURL(e.dataTransfer.files[0]))
+        let droppedFile = e.dataTransfer.files[0]
+        let sizeValidation = validateSize(droppedFile)
+        var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.tiff|\.tif)$/i;
 
-        let formData = new FormData()
+        if (!sizeValidation) {
+            setError('Please choose a file that is 1 mb or less.')
+            setTimeout(() => {
+                setError('')
+            },4000)
+            return false
+        } else if (!allowedExtensions.exec(e.dataTransfer.files[0].name)) {
+            setError("File type not supported. Please choose an image file such as jpeg, jpg, or png.")
+            setTimeout(() => {
+                setError('')
+            },4000)
+            return false
+        } else {
+            
+            let imageFile = e.dataTransfer.files
+        
+            setPreviewUrl(URL.createObjectURL(e.dataTransfer.files[0]))
+    
+            let formData = new FormData()
+    
+            formData.append("file", imageFile[0])
+            setError('')
+            axiosInstance.post("/upload_file", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+                onUploadProgress: data => {
+                    //Set the progress value to show the progress bar
+                    setProgress(Math.round((100 * data.loaded) / data.total))
+                    setIsLoading(true)
+    
+                },        
+            })
+            .catch(error => {
+                const { code } = error?.response?.data
+                switch (code) {
+                    case "FILE_MISSING":
+                        setError("Please select a file before uploading")
+                        break
+                    case "LIMIT_FILE_SIZE":
+                        setError("File size is too large. Please upload files below 1MB!")
+                        break
+                    case "INVALID_TYPE":
+                        setError(
+                            "This file type is not supported! Only .png, .jpg, and .jpeg files are allowed!"
+                        )
+                        break
+                        
+                    default:
+                        setError("Sorry, something went wrong. Please try again later.")
+                        break
+                }
+            })
 
-        formData.append("file", imageFile[0])
-
-        axiosInstance.post("/upload_file", formData, {
-            headers: {
-                "Content-Type": "multipart/form-data",
-            },
-            onUploadProgress: data => {
-                //Set the progress value to show the progress bar
-                setProgress(Math.round((100 * data.loaded) / data.total))
-                setIsLoading(true)
-
-            },        
-        })
+        }
     }
 
     const handleOndragOver = e => {
@@ -87,8 +171,6 @@ const DragAndDrop = (props) => {
     
   return (
       <div className="box-Wrapper">
-           
-
         {uploaded ?    
                 <div className="uploaded-wrapper">
                     <div className="uploaded-success-message-wrapper">
@@ -107,7 +189,7 @@ const DragAndDrop = (props) => {
                 </div>
             :
             (<>
-                {isLoading ? 
+                {!error && isLoading ? 
                     (<>
                         <h4>Uploading...</h4>
                         <ProgressBar className="progress-bar" now={progress} label={`${progress}%`} />
@@ -137,7 +219,7 @@ const DragAndDrop = (props) => {
                 }
             </>)
         }
-
+        {error && <Alert className="alert" variant="danger">{error}</Alert>}
     </div>
   );
 };
